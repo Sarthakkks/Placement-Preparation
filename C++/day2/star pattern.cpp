6e46ff1db02fcd3d40c9d4fc0a0a63f09b975201
Sarthak Bajpai <sarthakbajpai.1967@gmail.com>
2026-07-08 19:21:11 +0530

    Day 2: Solve right-aligned and pyramid pattern printing

--- C++/day2/star pattern.cpp
+++ C++/day2/star pattern.cpp
@@ -54,5 +54,17 @@ int main(){
         }
         cout<<endl;
     }
+    cout<<endl;
+    //pyramid from center
+    for(int i=1;i<=5;i++){
+        for(int j=5;j>i;j--){
+            cout<<" ";
+        }
+        for(int k=1;k<=2*i-1;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
     return 0;
 }
