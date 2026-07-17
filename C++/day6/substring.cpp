a7e54540c2937ef73080bfc7fce0eb22d1021c39
Sarthak Bajpai <sarthakbajpai.1967@gmail.com>
2026-07-17 15:19:18 +0530

    Updated day 6 Done with strings

--- C++/day6/substring.cpp
+++ C++/day6/substring.cpp
@@ -6,11 +6,16 @@ int main(){
     bool found = false;
     cout<<"Enter the Main string and Substring : ";
     cin>>main>>sub;
-    for(int i=0;i<main.length();i++){
+    for(int i=0;i<=main.length()-sub.length();i++){
+        found=true;
         for(int j=0;j<sub.length();j++){
-            if(main[i + j] == sub[j]){
-                found=true;
+            if(main[i + j] != sub[j]){
+                found=false;
+                break;
             }
+        }
+        if(found == true){
+            break;
         }
     }
     if(found==true){
